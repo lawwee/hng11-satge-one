ed490d24193f2ebb61e736049bf379f366044eb0
Lawwee <lawalmohammed567@gmail.com>
2024-07-03 23:21:04 +0100

    visitor update

--- controller.js
+++ controller.js
@@ -6,10 +6,10 @@ const api_key = process.env.API_KEY;
 
 exports.user_location = async (req, res) => {
     try {
+        const { visitor_name } = req.query;
         const clientIp = requestIp.getClientIp(req);
 
-        const ip = "102.88.83.44";
-        const geo = await axios.get(`http://ip-api.com/json/${ip}`);
+        const geo = await axios.get(`http://ip-api.com/json/${clientIp}`);
         const { city } = geo.data;
 
         const weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=no`);
@@ -17,9 +17,9 @@ exports.user_location = async (req, res) => {
         const { temp_c } = weather.data.current;
 
         res.status(200).json({
-            client_ip: ip,
+            client_ip: clientIp,
             location: city,
-            greeting: `Hello, Human!, the temperature is ${temp_c} degrees Celsius in ${city}`
+            greeting: `Hello, ${visitor_name}!, the temperature is ${temp_c} degrees Celsius in ${city}`
         });
     } catch (e) {
         console.error("Error getting user details: ", e);
